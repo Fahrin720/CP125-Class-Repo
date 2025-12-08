043fcd9b9aea9889ae38e0d4fff150b7ed53fced
Fahrin720 <muneerfahrin@gmail.com>
2025-12-08 12:53:29 +0800

    Real Complete Lab 2: Functions

--- labs/lab02/exercise2/exercise2.py
+++ labs/lab02/exercise2/exercise2.py
@@ -5,7 +5,7 @@ import math
 
 def calculate_event_cost(participants, tent_capacity, tent_price, meal_price):
     if tent_capacity > 0:
-        tent_cost = (int((participants / tent_capacity)) * tent_price) 
+        tent_cost = math.ceil((participants / tent_capacity)) * tent_price
     else:
         tent_cost = 0
     cost = (meal_price * participants) + tent_cost
@@ -13,3 +13,16 @@ def calculate_event_cost(participants, tent_capacity, tent_price, meal_price):
 
 result = calculate_event_cost(47, 6, 100, 25)
 print(f"Testing Camping Logistics: {result:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
